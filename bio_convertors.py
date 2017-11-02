891c1439847a6bab9a4d4f39ca3f25b30e95e989
Mohamed Hamidat <mohamed.hamidat@avanade.com>
2017-11-02 13:38:56 +0000

    replace T by U in dna sequence

--- bio_convertors.py
+++ bio_convertors.py
@@ -2,5 +2,5 @@ DNA_RNA_PAIRS = {"A" : "U", "C": "G", "G" : "C", "T" : "A"}
 def dna_to_rna(sequence):
     if sequence.startswith(">"):
         sequence = sequence[1:]
-    return sequence.upper().replace('U', 'T')
+    return sequence.upper().replace('T', 'U')
     
