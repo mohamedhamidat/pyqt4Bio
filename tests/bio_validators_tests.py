916f9828b5475d7022a3e2d96d783036070206c0
Mohamed Hamidat <mohamed.hamidat@avanade.com>
2017-11-02 19:38:44 +0000

    added unit test for bio_validators module

---
+++ tests/bio_validators_tests.py
@@ -0,0 +1,40 @@
+import unittest
+import sys
+sys.path.insert(0, "../src")
+from bio_validators  import is_valid_dna
+
+class BiovalidatorsTests(unittest.TestCase):
+    
+    def test_is_valid_dna_should_raise_ValueErrorException_when_string_is_empty(self):
+        with self.assertRaises(ValueError) as error:
+            is_valid_dna("")
+        self.assertEqual(error.exception.args[0], 'sequence should not be null or empty')
+    
+    def test_is_valid_dna_should_pairs_raise_ValueErrorException_when_string_isnot_valid(self):
+        with self.assertRaises(ValueError) as error:
+           is_valid_dna(123)
+        self.assertEqual(error.exception.args[0], 'sequence has to be a valid string')
+
+    def test_is_valid_dna_should_pairs_raise_ValueErrorException_when_superior_sign_missing(self):
+        with self.assertRaises(ValueError) as error:
+           is_valid_dna("AGCT")
+        self.assertEqual(error.exception.args[0], 'sequence should start with >')
+    
+    def test_is_valid_dna_should_pairs_raise_ValueErrorException_when_seq_contains_invalid_base(self):
+        with self.assertRaises(ValueError) as error:
+           is_valid_dna(">AGCn")
+        self.assertEqual(error.exception.args[0], 'sequence must have valid base')
+    
+    def test_is_valid_dna(self):
+        # Arrange
+        sequence = ">AGTC"
+        # Act 
+        result = is_valid_dna(sequence)
+        #Assert
+        self.assertEqual(result, True)
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
